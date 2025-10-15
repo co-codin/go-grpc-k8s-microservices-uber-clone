eea20c6fa806dbfb85ea7fc0ac0eae32a63897de
elijah <cuiyeqing960904@gmail.com>
2025-10-15 18:56:10 +0300

    making trip domain + repo + service

---
+++ services/trip-service/internal/infrastructure/repository/inmem.go
@@ -0,0 +1,12 @@
+package repository
+
+import "ride-sharing/services/trip-service/internal/domain"
+
+type inmemRepository struct {
+	trips     map[string]*domain.TripModel
+	rideFares map[string]*domain.RideFareModel
+}
+
+func NewInmemRepository() *inmemRepository {
+	
+}
