63fd2a369d611b71d59fa4c30246283a49c8d061
elijah <cuiyeqing960904@gmail.com>
2025-10-15 23:55:05 +0300

    frontend

--- services/api-gateway/main.go
+++ services/api-gateway/main.go
@@ -14,10 +14,19 @@ var (
 func main() {
 	log.Println("Starting API Gateway")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("Hello from API Gateway"))
 	})
 
-	http.ListenAndServe(httpAddr, nil)
+	server := &http.Server{
+		Addr:    httpAddr,
+		Handler: mux,
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Printf("HTTP server error: %v", err)
+	}
 }
