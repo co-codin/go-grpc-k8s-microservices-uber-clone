f9ab7c29b508d18ba64c74be8598ae798b29c7fa
elijah <cuiyeqing960904@gmail.com>
2025-10-17 12:51:02 +0300

    ws skeleton built

--- services/api-gateway/ws.go
+++ services/api-gateway/ws.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"ride-sharing/shared/contracts"
+	"ride-sharing/shared/util"
 
 	"github.com/gorilla/websocket"
 )
@@ -13,9 +16,81 @@ var upgrader = websocket.Upgrader{
 }
 
 func handleRidersWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		http.Error(w, "Could not open websocket connection", http.StatusBadRequest)
+		return
+	}
+	defer conn.Close()
 
+	userID := r.URL.Query().Get("userID")
+	if userID == "" {
+		http.Error(w, "userID is required", http.StatusBadRequest)
+		return
+	}
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Printf("Error reading message: %v", err)
+			break
+		}
+
+		log.Printf("Received message: %s", message)
+	}
 }
 
 func handleDriversWebSocket(w http.ResponseWriter, r *http.Request) {
-	
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		http.Error(w, "Could not open websocket connection", http.StatusBadRequest)
+		return
+	}
+	defer conn.Close()
+
+	userID := r.URL.Query().Get("userID")
+	if userID == "" {
+		http.Error(w, "userID is required", http.StatusBadRequest)
+		return
+	}
+
+	packageSlug := r.URL.Query().Get("packageSlug")
+	if packageSlug == "" {
+		http.Error(w, "packageSlug is required", http.StatusBadRequest)
+		return
+	}
+
+	type Driver struct {
+		Id             string `json:"id"`
+		Name           string `json:"name"`
+		ProfilePicture string `json:"profilePicture"`
+		CarPlate       string `json:"carPlate"`
+		PackageSlug    string `json:"PackageSlug"`
+	}
+
+	msg := contracts.WSMessage{
+		Type: "driver.cmd.register",
+		Data: Driver{
+			Id:             userID,
+			Name:           "test name",
+			ProfilePicture: util.GetRandomAvatar(1),
+			CarPlate:       "ABC123",
+			PackageSlug:    packageSlug,
+		},
+	}
+
+	if err := conn.WriteJSON(msg); err != nil {
+		log.Printf("Error sending message: %v", err)
+		return
+	}
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Printf("Error reading message: %v", err)
+			break
+		}
+
+		log.Printf("Received message: %s", message)
+	}
 }
