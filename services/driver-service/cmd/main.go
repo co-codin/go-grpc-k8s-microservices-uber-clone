73bb36e55f756c255735511f32a78e4249933dd3
elijah <cuiyeqing960904@gmail.com>
2025-10-21 15:23:12 +0300

    building  driver proto

--- services/driver-service/cmd/main.go
+++ services/driver-service/cmd/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	grpcserver "google.golang.org/grpc"
+)
+
+var GrpcAddr = ":9092"
 
 func main() {
-	fmt.Println("driver service")
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		sigCh := make(chan os.Signal, 1)
+		signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+		<-sigCh
+		cancel()
+	}()
+
+	lis, err := net.Listen("tcp", GrpcAddr)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpcserver.NewServer()
+
+
+	log.Printf("Starting gRPC server driver service on port: %s", lis.Addr().String())
+
+	go func() {
+		if err := grpcServer.Serve(lis); err != nil {
+			log.Printf("failed to serve: %v", err)
+			cancel()
+		}
+	}()
+
+	<-ctx.Done()
+	log.Println("Shutting down the server")
+	grpcServer.GracefulStop()
+
+}
+
+func cancel() {
+
 }
