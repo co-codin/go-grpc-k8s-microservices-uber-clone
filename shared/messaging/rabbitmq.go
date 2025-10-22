ae29ae76ed2da8347002e8370926ba94eff2aa2b
elijah <cuiyeqing960904@gmail.com>
2025-10-22 18:25:24 +0300

    amqp channel added

--- shared/messaging/rabbitmq.go
+++ shared/messaging/rabbitmq.go
@@ -7,7 +7,8 @@ import (
 )
 
 type RabbitMQ struct {
-	conn *amqp.Connection
+	conn    *amqp.Connection
+	Channel *amqp.Channel
 }
 
 func NewRabbitMQ(uri string) (*RabbitMQ, error) {
@@ -16,8 +17,15 @@ func NewRabbitMQ(uri string) (*RabbitMQ, error) {
 		return nil, fmt.Errorf("failed to connect to rabbitmq: %v", uri)
 	}
 
+	ch, err := conn.Channel()
+	if err != nil {
+		conn.Close()
+		return nil, fmt.Errorf("fail to create channel: %v", err)
+	}
+
 	return &RabbitMQ{
-		conn: conn,
+		conn:    conn,
+		Channel: ch,
 	}, nil
 }
 
@@ -25,4 +33,8 @@ func (r *RabbitMQ) Close() {
 	if r.conn != nil {
 		r.conn.Close()
 	}
+	if r.Channel != nil {
+		r.Channel.Close()
+	}
 }
+
